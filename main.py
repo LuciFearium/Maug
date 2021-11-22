79d09e732e2c71a74ec8f3bcff484285e8012158
LuciFearium <61132760+LuciFearium@users.noreply.github.com>
2021-11-22 18:55:03 +0000

    First function of bot, echo

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ print("Password is correct")
 
 
 
+print("Hello! Welcome to MaugBot! I am a virtual chatbot for your enjoyment!")
+
+print("With MaugBot, we have a few options! The first option is an Echo Bot! I will simply Echo what you say.")
+
+echoWord = input("What you like me to repeat?")
+
+print("You said: " + echoWord)
